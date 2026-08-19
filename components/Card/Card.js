20c00cc545ab4e4e9986e4cf4f83c614b0eb8529
nicole-002 <modesto.m.nicole@gmail.com>
2026-08-19 10:33:35 -0300

    .

---
+++ components/Card/Card.js
@@ -0,0 +1,23 @@
+const projetos = [
+    {
+        nome: "Nome do projeto",
+        subtitulo: "Subtítulo",
+        curso: "Informática para Internet",
+        sala: "2A",
+        bloco: "A",
+        imagem: "img/projeto1.png",
+        ods: [4, 9],
+        link: "projeto1.html"
+    },
+
+    {
+        nome: "Outro projeto",
+        subtitulo: "Outro subtítulo",
+        curso: "Administração",
+        sala: "3B",
+        bloco: "B",
+        imagem: "img/projeto2.png",
+        ods: [8, 12],
+        link: "projeto2.html"
+    }
+];
