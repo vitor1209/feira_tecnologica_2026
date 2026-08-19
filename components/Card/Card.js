8c365e77924ab7b4926a51285ee7db2a20122cf0
nicole-002 <modesto.m.nicole@gmail.com>
2026-08-19 10:44:51 -0300

    cardProjeto

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,23 +1,61 @@
-const projetos = [
-    {
-        nome: "Nome do projeto",
-        subtitulo: "Subtítulo",
-        curso: "Informática para Internet",
-        sala: "2A",
-        bloco: "A",
-        imagem: "img/projeto1.png",
-        ods: [4, 9],
-        link: "projeto1.html"
-    },
+function criarODS(numero) {
+    return `
+        <span class="ods-tag ods-${numero}">
+            ODS ${numero}
+        </span>
+    `;
+}
 
-    {
-        nome: "Outro projeto",
-        subtitulo: "Outro subtítulo",
-        curso: "Administração",
-        sala: "3B",
-        bloco: "B",
-        imagem: "img/projeto2.png",
-        ods: [8, 12],
-        link: "projeto2.html"
-    }
-];
+function criarListaODS(listaODS) {
+    return listaODS
+        .map(ods => criarODS(ods))
+        .join("");
+}
+
+function criarCard(projeto) {
+
+    return `
+        <article class="card-projeto">
+
+            <div class="card-imagem-container">
+
+                <img
+                    src="${projeto.imagem}"
+                    alt="${projeto.nome}"
+                    class="card-imagem"
+                >
+
+                <div class="card-ods">
+                    ${criarListaODS(projeto.ods)}
+                </div>
+
+            </div>
+
+            <div class="card-conteudo">
+
+                <h2 class="card-titulo">
+                    ${projeto.nome}
+                </h2>
+
+                <p class="card-subtitulo">
+                    ${projeto.subtitulo}
+                </p>
+
+                <p class="card-curso">
+                    ${projeto.curso}
+                </p>
+
+                <div class="card-local">
+                    <span>Sala: ${projeto.sala}</span>
+                    <span>Bloco: ${projeto.bloco}</span>
+                </div>
+
+                <a href="${projeto.link}" class="card-botao">
+                    Ver detalhes
+                </a>
+
+            </div>
+
+        </article>
+    `;
+}
---
+++ pages/Projetos/Projetos.js
@@ -0,0 +1,31 @@
+const projetos = [
+
+    {
+        nome: "Projeto Orion",
+        subtitulo: "Luva tecnológica musical",
+        curso: "Informática para Internet",
+        sala: "2A",
+        bloco: "A",
+        imagem: "../../img/orion.jpg",
+        ods: [4, 9],
+        link: "#"
+    },
+
+    {
+        nome: "EcoTech",
+        subtitulo: "Tecnologia sustentável",
+        curso: "Administração",
+        sala: "3B",
+        bloco: "B",
+        imagem: "../../img/ecotech.jpg",
+        ods: [11, 12],
+        link: "#"
+    }
+
+];
+
+const container = document.getElementById("lista-projetos");
+
+projetos.forEach(projeto => {
+    container.innerHTML += criarCard(projeto);
+});
