bc52552c6da86f4363430e85db764c20b23e887e
nicole-002 <modesto.m.nicole@gmail.com>
2026-08-26 10:51:47 -0300

    projeto filtro

--- components/ODS/OdsTags.js
+++ components/ODS/OdsTags.js
@@ -1,13 +1,18 @@
 function criarODS(numero) {
+
     return `
         <span class="ods-tag ods-${numero}">
             ODS ${numero}
         </span>
     `;
+
 }
 
+
 function criarListaODS(listaODS) {
+
     return listaODS
-        .map(numero => criarODS(numero))
+        .map(ods => criarODS(ods))
         .join("");
+
 }
