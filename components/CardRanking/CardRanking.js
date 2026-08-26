992e7e796b1b7d9fe9a5f4c49f3edb764dc78d47
thaisqabe <thaisq.abe@gmail.com>
2026-08-26 10:44:30 -0300

    card ranking

--- components/CardRanking/CardRanking.js
+++ components/CardRanking/CardRanking.js
@@ -1,26 +1,47 @@
-const style = document.createElement("link");
 
-style.rel = "stylesheet";
-style.href = new URL("./CardRanking.css", import.meta.url);
-
-console.log(style.href);
-
-document.head.appendChild(style);
-
-export function CardRanking({ title, description, image, price }) {
+export function CardRanking({
+  colocacao,
+  nome,
+  ods,
+  sala,
+  bloco,
+  curtidas
+}) {
   const card = document.createElement("article");
 
-  card.classList.add("card");
+  card.classList.add("card-ranking");
 
   card.innerHTML = `
-    <img src="${image}" alt="${title}">
-    
-    <div class="card-content">
-      <h2>${title}</h2>
-      <p>${description}</p>
-      <strong>R$ ${price}</strong>
-    </div>
-  `;
+        <div class="ranking-numero">
+            ${colocacao}º
+        </div>
+
+        <div class="ranking-info">
+
+            <h2>${nome}</h2>
+
+            <div class="ranking-detalhes">
+
+                <div class="ranking-ods">
+                    ${ods.map(odsItem => `
+                        <span class="ods-tag">ODS ${odsItem}</span>
+                    `).join("")}
+                </div>
+
+                <span class="ranking-localizacao">
+                    Sala ${sala} Bloco ${bloco}
+                </span>
+
+            </div>
+
+        </div>
+
+        <div class="ranking-curtidas">
+            <span class="numero-curtidas">${curtidas}</span>
+            <i class="fa-solid fa-thumbs-up"></i>
+
+        </div>
+    `;
 
   return card;
 }
--- pages/Ranking/Ranking.js
+++ pages/Ranking/Ranking.js
@@ -1,17 +1,38 @@
 import { CardRanking } from "../../components/CardRanking/CardRanking.js";
-import "../../components/CardRanking/CardRanking.css";
-
-
 
 const productsContainer = document.querySelector("#products");
 
-const product = {
-  title: "Notebook",
-  description: "Notebook para trabalho e estudos",
-  image: "./images/notebook.jpg",
-  price: "3.499,90"
-};
+const projetos = [
+    {
+        colocacao: 1,
+        nome: "Horta Inteligente",
+        ods: [2, 4, 12],
+        sala: 3,
+        bloco: "A",
+        curtidas: 128
+    },
 
-const card = CardRanking(product);
+    {
+        colocacao: 2,
+        nome: "Energia Solar Sustentável",
+        ods: [7, 11, 13],
+        sala: 5,
+        bloco: "B",
+        curtidas: 104
+    },
 
-productsContainer.appendChild(card);
+    {
+        colocacao: 3,
+        nome: "Robô Reciclador",
+        ods: [9, 12, 13],
+        sala: 2,
+        bloco: "A",
+        curtidas: 87
+    }
+];
+
+projetos.forEach(projeto => {
+    const card = CardRanking(projeto);
+
+    productsContainer.appendChild(card);
+});
